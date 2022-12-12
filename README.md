a58a5cbd6e36868b2d1f52c508ccd66a5fbaa20b
cleciopinheiro <cleciosantosdeveloper@gmail>
2022-12-12 16:15:37 -0300

    atualizando exercícios

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ O programa conta com mais de 1500 horas de aulas presenciais e online, aborda in
 
 - [x] 5.1: JavaScript - DOM e seletores
 - [x] 5.2: JavaScript - Trabalhando com elementos
-- [ ] 5.3: JavaScript - Eventos
+- [x] 5.3: JavaScript - Eventos
 - [ ] 5.4: JavaScript - Web Storage
 - [ ] 5.5: Fundamentos - JavaScript - Projetos
 - [ ] 5.5: [Projeto - Arte com Pixels]
--- fundamentos/secao-05-dom-eventos-webstorage/dia-03-eventos/exercicio/script.js
+++ fundamentos/secao-05-dom-eventos-webstorage/dia-03-eventos/exercicio/script.js
@@ -42,5 +42,153 @@ const createDaysOfTheMonth = () => {
       getDaysList.appendChild(dayItem);
     }
   }
-}
+};
 createDaysOfTheMonth();
+
+const createHolidayButton = (buttonName) => {
+    let buttonContainer = document.querySelector('.buttons-container');
+    let newButton = document.createElement('button');
+    let newButtonID = 'btn-holiday';
+  
+    newButton.innerHTML = buttonName;
+    newButton.id = newButtonID;
+    
+    buttonContainer.appendChild(newButton);
+  };
+  createHolidayButton('Feriados');
+
+const displayHolidays = () => {
+let getHolidayButton = document.querySelector('#btn-holiday');
+let getHolidays = document.querySelectorAll('.holiday')
+let backgroundColor = 'rgb(238,238,238)';
+let setNewColor = 'white';
+
+getHolidayButton.addEventListener('click', () => {
+    for (let index = 0; index < getHolidays.length; index += 1) {
+    if (getHolidays[index].style.backgroundColor === setNewColor) {
+        getHolidays[index].style.backgroundColor = backgroundColor;
+    } else {
+        getHolidays[index].style.backgroundColor = setNewColor;
+    }
+    }
+});
+}
+displayHolidays();
+
+const displayFridays = (fridaysArray) => {
+let getFridayButton = document.querySelector('#btn-friday');
+let fridays = document.getElementsByClassName('friday');
+let newFridayText = 'SEXTOU o/';
+
+getFridayButton.addEventListener('click', () => {
+for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerHTML !== newFridayText) {
+        fridays[index].innerHTML = newFridayText;
+        //caso o texto não tenha sido substituído, ao clicar no botão ele será substituído pelo novo texto;
+    } else {
+        fridays[index].innerHTML = fridaysArray[index];
+        //caso o texto já tenha sido substituído, ao clicar no botão ele volta ao texto padrão.
+    }
+    }
+});
+}
+let decemberFridays = [ 4, 11, 18, 25 ];
+displayFridays(decemberFridays);
+
+const dayMouseOver = () => {
+    let days = document.querySelector('#days');
+    days.addEventListener('mouseover', (event) => {
+      event.target.style.fontSize = '30px';
+      event.target.style.fontWeight = '600'; // Ele pega o evento alvo e altera o estilo de fontWeight para 600
+    });
+  }
+  
+  const dayMouseOut = () => {
+    let days = document.querySelector('#days');
+    days.addEventListener('mouseout', (event) => {
+      event.target.style.fontSize = '20px';
+      event.target.style.fontWeight = '200'; // Ele pega o evento alvo e altera o estilo de fontWeight para 200
+    });
+  }
+  dayMouseOver();
+  dayMouseOut();
+
+  const newTaskSpan = (task) => {
+    let tasksContainer = document.querySelector('.my-tasks');
+    let taskName = document.createElement('span');
+  
+    taskName.innerHTML = task;
+    tasksContainer.appendChild(taskName); // Anexando a task como filha de taskContainer
+  }
+  newTaskSpan('projeto');
+
+  const newTaskDiv = (color) => {
+    let tasksContainer = document.querySelector('.my-tasks');
+    let newTask = document.createElement('div');
+  
+    newTask.className = 'task';
+    newTask.style.backgroundColor = color;
+    tasksContainer.appendChild(newTask); // Adiciona newTask como filho de tasksContainer
+  }
+  newTaskDiv('green');
+
+  const setTaskClass = () => {
+    let selectedTask = document.getElementsByClassName('task selected');
+    let myTasks = document.querySelector('.task');
+    myTasks.addEventListener('click', (event) => {
+      if (selectedTask.length === 0) {
+        event.target.className = 'task selected';
+      } else {
+        event.target.className = 'task';
+      }
+    });
+  }
+  setTaskClass();
+
+  const setDayColor = () => {
+    let selectedTask = document.getElementsByClassName('task selected');
+    let days = document.querySelector('#days');
+    let taskDiv = document.querySelector('.task');
+    let taskColor = taskDiv.style.backgroundColor;
+    
+    days.addEventListener('click', (event) => {
+      let eventTargetColor = event.target.style.color;
+      if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+        let color = selectedTask[0].style.backgroundColor; // Pega a cor de fundo do primeiro elemento salvo na variável "selectedTask" e salva na variável "color"
+        event.target.style.color = color; // atribui a cor salva na variável "color" ao evento alvo
+      } else if (eventTargetColor === taskColor) {
+        event.target.style.color = 'rgb(119,119,119)';  // Altera a cor de fundo do evento alvo para "rgb(119, 119, 119)"
+      }
+    });
+  }
+setDayColor();
+
+const addNewTask = () => {
+    let getInputField = document.querySelector('#task-input');
+    let addInputButton = document.querySelector('#btn-add');
+    let getTaskList = document.querySelector('.task-list');
+  
+    addInputButton.addEventListener('click', () => {
+      if (getInputField.value.length > 0) {
+        let newLi = document.createElement('li');
+        newLi.innerText = getInputField.value;
+  
+        getTaskList.appendChild(newLi);
+        getInputField.value = '';
+      } else {
+        alert('Error: Digite ao menos 1 caractere.');
+      }
+    });
+  
+    getInputField.addEventListener('keyup', (event) => {
+      if (event.key === 'Enter' && getInputField.value.length > 0) {
+        let newLi = document.createElement('li');
+        newLi.innerText = getInputField.value;
+  
+        getTaskList.appendChild(newLi);
+        getInputField.value = '';
+      }
+    });
+  }
+  addNewTask();
+  
