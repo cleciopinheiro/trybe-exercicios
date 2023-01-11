ae91ff6f60e7482f25ee9c85df5ff665454b4688
cleciopinheiro <cleciosantosdeveloper@gmail.com>
2023-01-11 19:18:50 -0300

    Exercícios 5.2

---
+++ fundamentos/secao-05-html-css-forms/dia-02-bibliotecas-framework/script.js
@@ -0,0 +1,33 @@
+const submitBtn = document.querySelector('#btn-submit');
+const agreement = document.querySelector('#flexCheckChecked');
+
+const enableSubmit = () => {
+    const agreement = document.querySelector('#flexCheckChecked');
+    submitBtn.disabled = !agreement.checked;
+}
+
+agreement.addEventListener('change', enableSubmit);
+
+const textValidation = () => { 
+    const nameInput = document.querySelector('#name').value.length;
+    const emailInput = document.querySelector('#email').value.length;
+    const description = document.querySelector('#description').value.length;
+    if (emailInput < 10 || emailInput > 50 || nameInput < 10 || nameInput > 40 || description > 500) {
+      return false;
+    }
+    else {
+      return true;
+    }
+};
+
+const handlerSubmit = (event) => {
+    event.preventDefault();
+    const validation = textValidation();
+    if (validation === false) {
+      alert('Dados inválidos');
+    } else {
+      alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+    }
+}
+
+submitBtn.addEventListener('click', handlerSubmit);
