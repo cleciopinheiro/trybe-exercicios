7cd0aad38c5c1231a27f2872d29ea793f8111110
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-02-07 21:43:06 -0300

    Exercícios 7.4

---
+++ fundamentos/secao-07-hofs/dia-04-spread-rest/exercicio2.js
@@ -0,0 +1,21 @@
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const userInfos = {
+  ...user,
+  ...jobInfos,
+};
+
+// Aqui podemos desestruturar as chaves do objeto `userInfo` e então criar nossa mensagem diretamente pelas chaves desestruturadas.
+const { name, age, nationality, profession, squad, squadInitials } = userInfos;
+
+console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
