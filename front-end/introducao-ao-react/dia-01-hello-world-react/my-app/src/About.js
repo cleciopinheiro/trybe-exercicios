ce6f71d824f8a26457cf4343ada7c6c18b361407
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-02-27 16:09:48 -0300

    Exercicios 2.1

---
+++ front-end/introducao-ao-react/dia-01-hello-world-react/my-app/src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Clécio Pinheiro</h1>
+        <p>Estudante de Desenvolvimento Web</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Comunicativo</li>
+          <li>Proativo</li>
+          <li>Determinado</li>                    
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
