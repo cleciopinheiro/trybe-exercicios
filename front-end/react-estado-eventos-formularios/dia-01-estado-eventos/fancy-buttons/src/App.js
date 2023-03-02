475a0e65e64f92ba5c72ac8c27a723a9b27a9bc3
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-03-02 17:09:24 -0300

    Exercício 3.1

--- front-end/react-estado-eventos-formularios/dia-01-estado-eventos/fancy-buttons/src/App.js
+++ front-end/react-estado-eventos-formularios/dia-01-estado-eventos/fancy-buttons/src/App.js
@@ -2,25 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function handleClickFire() {
-  console.log('FOGO');
-}
-
-function handleClickIce() {
-  console.log('GELO');
-}
-
-function handleClickEarth() {
-  console.log('TERRA');
-}
-
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      clickFire: 0,
+      clickIce: 0,
+      clickEarth: 0
+    }
+  }
+
+  handleClickFire = () => {
+    this.setState((lastNumber) => ({
+      clickFire: lastNumber.clickFire + 1,
+    }));
+  }
+  
+  handleClickIce = () => {
+    this.setState((lastNumber) => ({
+      clickIce: lastNumber.clickIce + 1,
+    }));
+  }
+  
+  handleClickEarth = () => {
+    this.setState((lastNumber) => ({
+      clickEarth: lastNumber.clickEarth + 1,
+    }));
+  }
   render() {
     return (
       <div>
-        <button className="fire" onClick={ handleClickFire }>FOGO</button>
-        <button className="ice" onClick={ handleClickIce }>GELO</button>
-        <button className="earth" onClick={ handleClickEarth }>EARTH</button>
+        <button className="fire" onClick={ this.handleClickFire }>{ this.state.clickFire } FOGO</button>
+        <button className="ice" onClick={ this.handleClickIce }>{ this.state.clickIce } GELO</button>
+        <button className="earth" onClick={ this.handleClickEarth }>{ this.state.clickEarth } EARTH</button>
       </div>
     )
   }
